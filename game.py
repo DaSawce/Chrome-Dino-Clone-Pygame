1018a5e60259e25366bdb9fd8ea7701ddbce5ecb
David Romanillos <davidromanillos18@gmail.com>
2023-12-07 00:27:35 +0800

    yes

--- game.py
+++ game.py
@@ -28,8 +28,25 @@ SMALL_CACTUS = [pygame.image.load(os.path.join("Assets/Cactus", "SmallCactus1.pn
 LARGE_CACTUS = [pygame.image.load(os.path.join("Assets/Cactus", "SmallCactus1.png")),
            pygame.image.load(os.path.join("Assets/Cactus", "SmallCactus2.png")), pygame.image.load(os.path.join("Assets/Cactus", "SmallCactus3.png"))]
 
+CLOUD = pygame.image.load(os.path.join("Assets/Other", "Cloud.png"))
+
+BACKGROUND = pygame.image.load(os.path.join("Assets/Other", "Track.png"))
+
+
 
 
 
 def main():
+    run = True
+    clock=pygame.time.Clock()
+    player = Dinosaur()
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        SCREEN.fill((255, 255, 255))
+        userInput = pygame.key.get_pressed()
+
     pygame.display.update
