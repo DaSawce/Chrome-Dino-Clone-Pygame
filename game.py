62fe049ba3200164def24f288888f3f62171a8b3
David Romanillos <davidromanillos18@gmail.com>
2023-12-07 22:29:08 +0800

    cactus

--- game.py
+++ game.py
@@ -147,7 +147,13 @@ class SmallCactus(Obstacle):
     def __init__(self, image):
         self.type = random.randint(0, 2)
         super().__init__(image, self.type)
-        self.rect.y = 325       
+        self.rect.y = 325      
+
+class LargeCactus(Obstacle):
+    def __init__(self, image):
+        self.type = random.randint(0, 2)
+        super().__init__(image, self.type)
+        self.rect.y = 300 
 
 
         
