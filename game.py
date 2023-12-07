d167fc5cdce3e2c2a7b4a75840585e90f3d48a77
David Romanillos <davidromanillos18@gmail.com>
2023-12-07 22:28:52 +0800

    cactus

--- game.py
+++ game.py
@@ -141,7 +141,13 @@ class Obstacle:
             obstacles.pop()
 
     def draw(self, SCREEN):
-        SCREEN.blit(self.image[self.type], self.rect)       
+        SCREEN.blit(self.image[self.type], self.rect)
+
+class SmallCactus(Obstacle):
+    def __init__(self, image):
+        self.type = random.randint(0, 2)
+        super().__init__(image, self.type)
+        self.rect.y = 325       
 
 
         
